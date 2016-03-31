e3398d5914f62416fc7b9c542a95f4c8924eb1cf
Christoph Franke <franke@ragusescheer.de>
2016-03-31 12:37:25 +0200

    Fixed: Fehlerhafte Ausgabe

--- py/sync_files.py
+++ py/sync_files.py
@@ -5,10 +5,10 @@ from modules import sync
 
 @engine.prepare_and_clean
 def execute():
-    out.log("Synchronizing remote files...")
+    out.log("Synchronizing local files...")
     table = sync.download()
 
 
 
 def help():
-    out.log("This command donwloads all files, that are present on the remote but not on the local machines www dir.", 'help')
+    out.log("This command downloads all files, that are present on the remote but not on the local machines www dir.", 'help')
