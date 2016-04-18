918ff6277c2eb426b5c64cb2e38c6728b2ec6fb2
Christoph Franke <franke@ragusescheer.de>
2016-04-18 17:58:42 +0200

    pngcrush korrekt eingebunden

--- py/modules/image.py
+++ py/modules/image.py
@@ -22,9 +22,18 @@ def optimize_png():
     for root, dirnames, filenames in os.walk(engine.LOCAL_WWW_DIR):
         for filename in fnmatch.filter(filenames, '*.png'):
             original_name = root + '/' + filename
-            optimized_name = root + '/' + filename[:-4] + 'opt.png'
-            run.local('pngcrush ' + original_name + ' ' + optimized_name, halt_on_stderr = False)
+            optimized_name = root + '/' + filename[:-4] + '.opt.png'
+            run.local('pngcrush -q ' + original_name + ' ' + optimized_name, halt_on_stderr = False)
             if os.path.isfile(optimized_name):
-                run.local('rm ' + original_name)
-                run.local('mv ' + optimized_name + ' ' + original_name)
+                new_size = os.path.getsize(optimized_name)
+                old_size = os.path.getsize(original_name)
+                if new_size < old_size:
+                    out.log('reduced ' + original_name + ': ' + str(old_size) + ' --> ' + str(new_size), 'bytes (' + str(100-100*new_size/old_size) + '%)', 'image')
+                    run.local('rm ' + original_name)
+                    run.local('mv ' + optimized_name + ' ' + original_name)
+                else:
+                    out.log('could not reduce ' + original_name + ' any further, skipped.', 'image')
+                    run.local('rm ' + optimized_name)
+            else:
+                out.log('pngcrush did decide not to write an output file for ' + original_name, 'image')
 
