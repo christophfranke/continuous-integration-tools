2fd8c13bb53aee034871791a9d3f892f5a2e822b
Christoph Franke <franke@ragusescheer.de>
2016-07-05 14:46:07 +0200

    Allow retry on ssh command system, also do not hide output through verbose level out.log line

--- py/modules/ssh.py
+++ py/modules/ssh.py
@@ -5,10 +5,10 @@ import out
 @out.indent
 def execute(command):
     import run
-    out.log('executing ' + command, 'ssh', out.LEVEL_VERBOSE)
+    #out.log('executing ' + command, 'ssh', out.LEVEL_VERBOSE)
 
     #run the command
     #ssh_command_line = 'ssh ' + engine.escape(engine.SSH_USER) + ':' + engine.escape(engine.SSH_PASSWORD) + '@' + engine.SSH_HOST + " '" + command + "'"
     ssh_command_line = 'ssh ' + engine.SSH_USER + '@' + engine.SSH_HOST + " 'cd " + engine.SSH_PATH_TO_WWW_DIR + ' && ' + command + "'"
-    run.local(ssh_command_line)
+    run.local(ssh_command_line, retry = 3)
 
