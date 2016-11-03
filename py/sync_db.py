56c52284d43201ed5fda8ea660dc144dac4d2e0c
Christoph Franke <franke@ragusescheer.de>
2016-11-03 12:52:57 +0100

    create compressed remote dump and download normally instead of creating uncompressed dump and applying compression to the download. That way, the dump and the compression can be aggregated into one php command system call, thus reducing overhead

--- py/sync_db.py
+++ py/sync_db.py
@@ -8,8 +8,8 @@ def execute():
     if engine.LOCAL_DB_NAME is not None:
         out.log("Synchronizing local db...")
         mysql.export_local_db()
-        remote_dump = mysql.create_remote_dump()
-        local_dump = transfer.get_compressed(remote_dump)
+        remote_dump = mysql.create_remote_dump(compression = True)
+        local_dump = transfer.get(remote_dump)
         mysql.import_local_db(local_dump)
     else:
         out.log('No local database named, nothing to do.')
